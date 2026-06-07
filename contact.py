1b17e3f84ca0197309cb7a340195a032205d9dc3
zineb36-star <boumalekzineb36@gmail.com>
2026-06-07 19:38:34 +0100

    GDCMM-12 afficher liste contacts

--- contact.py
+++ contact.py
@@ -6,8 +6,7 @@ class Contact:
         assert isinstance(nom, str) and len(nom)>0, "Nom invalide"
         assert isinstance(email, str) and re.match(r'^[\w\.-]+@[\w\.-]+\.\w+$', email), "Email invalide"
         assert isinstance(numtele, str) and numtele.isdigit() and len(numtele) >= 10, "Téléphone invalide"
-        #assert isinstance(numtele, str) and numtele.isdigit() and len(numtele) >= 10
-        #
+        #assert isinstance(numtele, str) and numtele.isdigit() and len(numtele) >= 10, "Téléphone invalide"
         self.nom = nom
         self.email = email
         self.numtele = numtele
