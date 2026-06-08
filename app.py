7d3fea4000fa15079919bd6f3e5018c4f1b0f806
zineb36-star <boumalekzineb36@gmail.com>
2026-06-08 16:27:27 +0100

    feature/GDCMM-43-supprimer-rdv

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import urllib.parse
 from email.mime.text import MIMEText 
 from email.mime.multipart import MIMEMultipart
 
-app = Flask(__name__)
+app = Flask(__name__)  
  
 app.secret_key = "secret123"
 # routes 
