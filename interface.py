175c62fd91254347b2e448e54afc88c8d8d9a496
zineb36-star <boumalekzineb36@gmail.com>
2026-06-08 14:51:57 +0100

    GDCMM-28 password chiffre

--- interface.py
+++ interface.py
@@ -151,7 +151,7 @@ scrollbar = ttk.Scrollbar(
      
 scrollbar.pack(side=tk.RIGHT, fill="y")
 
-tree.configure(yscrollcommand=scrollbar.set)
+tree.configure(yscrollcommand=scrollbar.set)  
 # =========================
 # FORMULAIRE
 # =========================
